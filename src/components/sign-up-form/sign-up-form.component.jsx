5dc88d0e97ace8d060d89937eb5f2555b377caee
Chamath Jayarathne <chamathsocial@gmail.com>
2022-11-26 18:05:52 -0500

    feat[sign-up form]: Create Sign-up form

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,6 +1,11 @@
 import {useState} from 'react';
 
+import FormInput from '../form-input/form-input.component'; 
+import Button from '../button/button.component'; 
+
 import { createAuthUserWithEmailAndPassword, createUserDocFromAuth } from '../../utils/firebase/firebase.utils';
+
+import './sign-up-form.styles.scss';
 
 const defaultFormFields = {
     displayName: '',
@@ -48,11 +53,12 @@ const SignUpForm = () => {
     };
 
     return(
-        <div>
-            <h1>Sign up here</h1>
+        <div className='sign-up-container'>
+            <h2>Don't have an account?</h2>
+            <span>Sign up here</span>
             <form onSubmit={handleSubmit}>
-                <label htmlFor="">Display Name</label>
-                <input 
+                <FormInput
+                    label='Display Name'
                     type="text" 
                     required 
                     onChange={handleChange} 
@@ -60,8 +66,8 @@ const SignUpForm = () => {
                     value={displayName} 
                 />
                 
-                <label htmlFor="">Email</label>
-                <input 
+                <FormInput
+                    label='Email' 
                     type="email" 
                     required 
                     onChange={handleChange} 
@@ -69,8 +75,8 @@ const SignUpForm = () => {
                     value={email} 
                 />
 
-                <label htmlFor="">Password</label>
-                <input 
+                <FormInput
+                    label='Password' 
                     type="password" 
                     required 
                     onChange={handleChange} 
@@ -78,8 +84,8 @@ const SignUpForm = () => {
                     value={password} 
                 />
 
-                <label htmlFor="">Confirm Password</label>
-                <input 
+                <FormInput
+                    label='Confirm Password' 
                     type="password" 
                     required 
                     onChange={handleChange} 
@@ -87,7 +93,7 @@ const SignUpForm = () => {
                     value={confirmPassword} 
                 />
 
-                <button type="submit">Sign Up</button>
+                <Button type="submit">Sign Up</Button>
             </form>
         </div>
     )

d617de6c108ead75702c28ed2dcfaf6c5d0d5d88
Chamath Jayarathne <chamathsocial@gmail.com>
2022-12-14 10:17:01 -0500

    feat[User Context]: Use useReducer to replace useState in User Context

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,16 +1,43 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
 
 import { createUserDocFromAuth, onAuthStateChangedListener } from "../utils/firebase/firebase.utils";
 
-//actual value you need to access
 export const UserContext = createContext({
     currentUser: null,
     setCurrentUser: () => null
 });
 
-// the component
+export const USER_ACTION_TYPES = {
+    SET_CURRENT_USER: 'SET_CURRENT_USER'
+}
+
+const userReducer = (state, action) => {
+
+    const {type, payload} = action;
+
+    switch(type){
+        case USER_ACTION_TYPES.SET_CURRENT_USER:
+            return  {
+                ...state,
+                currentUser: payload
+            }
+        default:
+            throw new Error(`Unrecognized type in userReducer::: ${type}`);
+    }
+}
+
+const INITIAL_STATE = {
+    currentUser: null
+}
+
 export const UserProvider = ({children}) => {
-    const [currentUser, setCurrentUser] = useState(null);
+    const [state, dispatch] = useReducer(userReducer, INITIAL_STATE);
+    const {currentUser} = state;
+
+    const setCurrentUser = (user) => {
+        dispatch({type: USER_ACTION_TYPES.SET_CURRENT_USER, payload:user});
+    }
+
     const value = {currentUser, setCurrentUser};
 
     useEffect(() => {

eb2ed4a3bd11622b6b4a686818495f322b8b3993
Chamath Jayarathne <chamathsocial@gmail.com>
2022-12-15 19:28:05 -0500

    feat: Start migrating to Redux from Context

--- src/index.js
+++ src/index.js
@@ -5,24 +5,25 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import { BrowserRouter } from 'react-router-dom';
-import { UserProvider } from './contexts/user.context';
 import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
+import {Provider} from 'react-redux';
+import {store} from './store/store'
 
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
+    <Provider store={store}>
+      <BrowserRouter>
         <CategoriesProvider>
           <CartProvider>
             <App />
           </CartProvider>
         </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
